ab8d243835e32f0336b3cd3118a92786f4e6081b
Luca <lucamug@gmail.com>
2017-02-01 08:56:58 +0100

    no message

--- app/neural-network-test.js
+++ app/neural-network-test.js
@@ -1,3 +1,4 @@
+// node app/neural-network-test.js
 "use strict";
 
 // import numpy as np
@@ -58,6 +59,47 @@ const genArray = (x, y, fn) => {
     return r;
 };
 
+const operate = (a1, a2, fn) => {
+    const r = [];
+    a1.forEach((v, i) => {
+        r[i] = [fn(v, a2[i])];
+    });
+    return r;
+};
+
+const matrixOperate = (fn) => {
+    return (a1, a2) => {
+        const r = [];
+        a1.forEach((v, i) => {
+            r[i] = [fn(v, a2[i])];
+        });
+        return r;
+    };
+};
+
+const matrixAdd = matrixOperate((a, b) => {
+    return a + b;
+});
+const matrixSub = matrixOperate((a, b) => {
+    return a - b;
+});
+const matrixMul = matrixOperate((a, b) => {
+    return a * b;
+});
+
+
+const operate2 = (a1, a2, fn) => {
+    const r = [];
+    a1.forEach((v1, i) => {
+        v1.forEach((v2, j) => {
+            r[i] = r[i] || [];
+            r[i][j] = [fn(v2, a2[i][j])];
+        });
+    });
+    return r;
+};
+
+
 // # seed random numbers to make calculation
 // # deterministic (just a good practice)
 // np.random.seed(1)
@@ -85,6 +127,18 @@ syn0 = [
 // for iter in xrange(10000):
 // 
 
+const transpose = (a) => {
+    const r = [];
+    a.forEach((v1, i) => {
+        // console.log("xxxx", typeof v1);
+        v1.forEach((v2, j) => {
+            r[j] = r[j] || [];
+            r[j][i] = v2;
+        });
+    });
+    return r;
+};
+
 const dot = (a1, a2) => {
     const r = [];
     a1.forEach((v1, i1) => {
@@ -99,7 +153,7 @@ const dot = (a1, a2) => {
     return r;
 };
 
-let l1, l0, l1_error;
+let l1, l0, l1_error, l1_delta, temp;
 
 for (let iter = 0; iter < 1; iter++) {
 
@@ -110,22 +164,46 @@ for (let iter = 0; iter < 1; iter++) {
     l0 = X;
     l1 = nonlin(dot(l0, syn0));
 
+    console.log(l0);
+    console.log(syn0);
+    console.log(dot(l0, syn0));
+    console.log(nonlin(dot(l0, syn0)));
+
     //     # how much did we miss?
     //     l1_error = y - l1
 
-    l1_error = y - l1;
-
-    console.log(syn0);
-    console.log(y);
-    console.log(l1);
+    l1_error = operate(y, l1, (a, b) => {
+        return (a - b);
+    });
 
     //     # multiply how much we missed by the 
     //     # slope of the sigmoid at the values in l1
     //     l1_delta = l1_error * nonlin(l1,True)
-    // 
+
+    l1_delta = operate(l1_error, nonlin(l1, true), (a, b) => {
+        return (a * b);
+    });
+
     //     # update weights
     //     syn0 += np.dot(l0.T,l1_delta)
 
+    //console.log(transpose(l0));
+    //console.log(l1_delta);
+    //temp = dot(transpose(l0), l1_delta);
+    //console.log(temp);
+
+    // console.log(syn0);
+    syn0 = operate(syn0, dot(transpose(l0), l1_delta), (a, b) => {
+        return (a + b);
+    });
+
+
+    // console.log(syn0);
+    // console.log(l1_error);
+    //console.log(transpose(l0));
+    //console.log(l1_delta);
+    //console.log(dot(transpose(l0), l1_delta));
+    //console.log(syn0);
 }
 
 // 
--- app/temp.py
+++ app/temp.py
@@ -6,9 +6,7 @@ import numpy as np
 # sigmoid function
 def nonlin(x,deriv=False):
     if(deriv==True):
-        # return x*(1-x)
-        return "ciao"
-        
+        return x*(1-x)
     return 1/(1+np.exp(-x))
     
 # input dataset
@@ -23,16 +21,28 @@ np.random.seed(1)
 syn0 = 2*np.random.random((3,1)) - 1
 # syn0 = np.array([[0.5], [0], [-0.5]])
 
-print (syn0)
 
 for iter in range(1):
   l0 = X
   l1 = nonlin(np.dot(l0,syn0))
-  l1_error = y - l1;
-  print (y)
-  print (l1)
-  print (l1_error)
 
+  print (l0)
+  print (syn0)
+  print (np.dot(l0,syn0))
+  print (nonlin(np.dot(l0,syn0)))
+
+
+  l1_error = y - l1;
+  l1_delta = l1_error * nonlin(l1,True)
+  #print (syn0)
+  syn0 += np.dot(l0.T,l1_delta)
+
+  # print (l1)
+  #print (l1_error)
+  #print (l0.T)
+  #print (l1_delta)
+  #print (np.dot(l0.T,l1_delta))
+  #print (syn0)
 
 
 
